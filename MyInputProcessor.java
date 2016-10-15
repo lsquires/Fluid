7ca09668935a94e560cba167e0d4ecc0ee62fc02
Laurence <laurence.j.squires@gmail.com>
2016-10-15 17:30:15 +0100

    Updated to the correct version 2

---
+++ MyInputProcessor.java
@@ -0,0 +1,54 @@
+package com.mynameislaurence.fluid;
+
+import com.badlogic.gdx.InputProcessor;
+
+/**
+ * Created by Home on 25/06/2015.
+ */
+public class MyInputProcessor implements InputProcessor {
+    public Simulation sim;
+    public MyInputProcessor(Main main) {
+        sim = main.sim;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        sim.spawnParticles(screenX,screenY);
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
